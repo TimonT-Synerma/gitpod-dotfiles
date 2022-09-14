b43e684b6643320b2ce4a7a5264b4fffbd8b3b1f
Timon Tanner <timon.tanner@synerma.ch>
2022-09-14 11:05:58 +0200

    add variable to define ohmyzsh plugins

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Oh My Zsh plugins to install
+plugins=""git colorize zsh-sntax-highlighting"
+
+sed -i "/^plugins/s/plugins=\(.*\)/plugins=\($plugins\)/g" /home/gitpod/.zshrc
