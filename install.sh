7c7b2d558f40f12646b2edaeab6158c0fb86f545
Timon Tanner <timon.tanner@synerma.ch>
2022-09-14 12:24:36 +0200

    install plugins

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #Oh My Zsh plugins to install
 plugins="git colorize zsh-sntax-highlighting"
+
 aliases='alias dcd="docker compose -f docker-compose.dev.yml"
 alias dcdr="docker compose -f docker-compose.dev.yml run rails"
 alias dct="docker compose -f docker-compose.test.yml"'
@@ -17,5 +18,11 @@ echo $aliases >> /home/gitpod/.oh-my-zsh/custom/aliases.zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/gitpod/.oh-my-zsh/custom/themes/powerlevel10k
 #Write theme to .zshrc
 sed -i "/^ZSH_THEME=\"/s/.*/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" /home/gitpod/.zshrc
+
+#Install zsh-autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/gitpod/oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+#install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/gitpod/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 
